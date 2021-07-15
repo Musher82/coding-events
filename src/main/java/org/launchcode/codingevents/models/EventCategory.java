c0ba5a3b337112489cfc4dbbcb550aed661137d6
Micah <mrusher@gmail.com>
2021-07-15 03:00:17 -0500

    Added one to many functionality for event category

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,13 +1,21 @@
 package org.launchcode.codingevents.models;
 
+import org.springframework.validation.Errors;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class EventCategory extends AbstractEntity {
 
     @Size(min = 3, message = "Name must be at least 3 characters long")
-    public String name;
+    private String name;
+
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
 
     public EventCategory(@Size(min = 3, message = "Name must be at least 3 characters long") String name){
         this.name = name;
@@ -15,13 +23,16 @@ public class EventCategory extends AbstractEntity {
 
     public EventCategory() {}
 
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
     }
 
     @Override
